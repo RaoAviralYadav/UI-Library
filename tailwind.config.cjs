467de563126a6043f4e476701da1fe11ba3138cb
RaoAviralYadav <24f3000436@ds.study.iitm.ac.in>
2025-09-17 02:42:33 +0530

    Packages Updated

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./app/**/*.{ts,tsx,js,jsx,mdx}",
+    "./src/**/*.{ts,tsx,js,jsx,mdx}",
+    "./stories/**/*.{ts,tsx,js,jsx,mdx}",
+    "./.storybook/**/*.{ts,tsx,js,jsx,mdx}"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
